8c94cf8b5d37dd7688a81a36eb5a252ea4fd33a2
mostafaswifi <mostafakhederswifi@gmail.com>
2021-10-16 00:59:03 +0200

    card2

---
+++ js/myScrip.js
@@ -0,0 +1,23 @@
+$(function(){
+    
+        // $('section').fadeTo(3000,1)
+
+$(window).one('scroll',function(){
+    $('section').fadeTo(3000,1);
+    // alert("hello")
+    // $('#carouselExampleFade').fadeTo(3000,1)
+  });
+   
+
+
+
+
+
+
+
+
+  //carouselExampleFade
+})
+
+
+
